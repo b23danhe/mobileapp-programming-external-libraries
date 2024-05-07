acec4fb7fd9d27ecb86d78fb0ba189f198634af7
b23danhe <b23danhe@student.his.se>
2024-05-07 21:30:31 +0200

    Added Picasso library in Gradle and added a ImageView to the layout and MainActivity

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView picassoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        picassoView = findViewById(R.id.imageView);
+
     }
 }
