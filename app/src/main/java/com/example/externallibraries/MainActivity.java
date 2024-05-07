25267263787f7494328362adbcf1f950fa393d80
b23danhe <b23danhe@student.his.se>
2024-05-07 22:28:12 +0200

    Adding margin to ImageView and TextView

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -22,11 +22,9 @@ public class MainActivity extends AppCompatActivity {
         picassoView = findViewById(R.id.imageView);
 
         String url = "https://m.media-amazon.com/images/I/71FQu2QB0eL._SL1060_.jpg";
-        //String url = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/95f34c22-cbd5-486e-b58f-d9a55ff3c38f/df055xl-8b11f93f-9e60-4d39-a09d-682e76918699.jpg/v1/fill/w_900,h_900,q_75,strp/magic_horse_by_polychromika_df055xl-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvOTVmMzRjMjItY2JkNS00ODZlLWI1OGYtZDlhNTVmZjNjMzhmXC9kZjA1NXhsLThiMTFmOTNmLTllNjAtNGQzOS1hMDlkLTY4MmU3NjkxODY5OS5qcGciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.youUtEBzGV3vXWdtTuqVgLKoXzSNQwqLNvKEbjZfZQQ";
 
         Picasso.get()
                 .load(url)
                 .into(picassoView);
-
     }
 }
