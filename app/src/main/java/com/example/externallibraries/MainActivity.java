1f44d52524db0b781f3e5752591724fd94dabe70
b23danhe <b23danhe@student.his.se>
2024-05-08 10:09:29 +0200

    Added a last picture and made som final layout changes

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
     }
-    private ImageView picassoView, horseOneView, horseTwoView, horseThreeView;
+    private ImageView queenHorseView, horseOneView, horseTwoView, horseThreeView, horseFourView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,19 +33,21 @@ public class MainActivity extends AppCompatActivity {
                 .build());
         setContentView(R.layout.activity_main);
 
-        picassoView = findViewById(R.id.imageView);
+        queenHorseView = findViewById(R.id.queen_horse);
         horseOneView = findViewById(R.id.horse_one);
         horseTwoView = findViewById(R.id.horse_two);
         horseThreeView = findViewById(R.id.horse_three);
+        horseFourView = findViewById(R.id.horse_four);
 
         String url1 = "https://m.media-amazon.com/images/I/71FQu2QB0eL._SL1060_.jpg";
-        String url2 = "https://media.posterlounge.com/img/products/500000/491269/491269_poster.jpg";
         String url3 = "https://img.freepik.com/premium-photo/magical-unicorn-full-colors-so-many-details-magic-horse-from-fairy-tale_853115-4659.jpg?w=1380";
+        String url2 = "https://i.etsystatic.com/36531035/r/il/6200b0/5667645573/il_1588xN.5667645573_rnsx.jpg";
         String url4 = "https://i.etsystatic.com/36531035/r/il/2c24ab/5667645571/il_1588xN.5667645571_9mfn.jpg";
+        String url5 = "https://media.posterlounge.com/img/products/500000/491269/491269_poster.jpg";
 
         Picasso.get()
                 .load(url1)
-                .into(picassoView);
+                .into(queenHorseView);
 
         Picasso.get()
                 .load(url2)
@@ -58,5 +60,9 @@ public class MainActivity extends AppCompatActivity {
         Picasso.get()
                 .load(url4)
                 .into(horseThreeView);
+
+        Picasso.get()
+                .load(url5)
+                .into(horseFourView);
     }
 }
